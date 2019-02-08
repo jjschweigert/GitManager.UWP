37378d514071fba1662224dd75e4b6e30b3b1597
jjschweigert <jjschweigert.developer@gmail.com>
2019-02-07 19:08:15 -0500

    Updated ui

--- GitManager.UWP/MainPage.xaml.cs
+++ GitManager.UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,11 @@ namespace GitManager.UWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await new MessageDialog("asdfadfasdf").ShowAsync();
         }
     }
 }
