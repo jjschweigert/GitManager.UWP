e4f571f8c7f301d6e65c16794f338f93129a9e2d
jjschweigert <jjschweigert.developer@gmail.com>
2019-05-24 13:43:23 -0400

    Added local repo page modifications

---
+++ GitManager.UWP/ViewModel/LocalViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitManager.UWP.ViewModel
+{
+    public class LocalViewModel
+    {
+        private Action _Action1 { get; set; }
+        public Action Action1
+        {
+            get
+            {
+                return _Action1;
+            }
+            set
+            {
+                Debug.WriteLine("Updated LocalViewModel with new action Action1");
+                _Action1 = value;
+            }
+        }
+    }
+}
